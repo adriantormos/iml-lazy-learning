cf6814b59b49fded2c313b33b4f04fc350f1014b
ferranagullolopez <ferranagullolopez@gmail.com>
2020-12-19 16:38:00 +0100

    deletes splice dataset

--- src/factory/dataset.py
+++ src/factory/dataset.py
@@ -1,6 +1,5 @@
 from src.data.dataset import Dataset
 from src.data.types.hypothyroid import HypothyroidDataset
-from src.data.types.splice import SpliceDataset
 
 
 class DatasetFactory:
@@ -13,8 +12,6 @@ class DatasetFactory:
         name = config['name']
         if name == 'hypothyroid':
             dataset = HypothyroidDataset(config, verbose)
-        elif name == 'splice':
-            dataset = SpliceDataset(config, verbose)
         else:
             raise Exception('The dataset with name ' + name + ' does not exist')
         if issubclass(type(dataset), Dataset):
